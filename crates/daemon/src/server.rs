02f21820f9888b5f56acf37d2c0bed12734bed32
Paul Planchon <paul@planchon.io>
2026-03-22 15:27:46 +0100

    make vite works

--- crates/daemon/src/server.rs
+++ crates/daemon/src/server.rs
@@ -95,7 +95,9 @@ pub async fn handle_request(
     println!("trying to connect to {}, {}", addr, method);
 
     // bi directionnal tunnel for websocket and that stuffs
-    if method == Method::CONNECT {
+    if method == Method::CONNECT || req.headers().contains_key("Upgrade") {
+        println!("Upgrading the connection");
+
         tokio::task::spawn(async move {
             match hyper::upgrade::on(req).await {
                 Ok(upgraded) => {
